e3194253b080a078dcee2cd8805471fb1e483e46
brunoscholz <bruno@oction.com.br>
2017-12-07 13:25:54 -0200

    refactored navbar, message count done

--- frontend/src/app/chat/chat.module.ts
+++ frontend/src/app/chat/chat.module.ts
@@ -5,24 +5,29 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { ChatWindowComponent } from './chat-window.component';
 import { ChatMessageComponent } from './chat-message.component';
-//import { ChatRoutingModule } from './chat-routing.module';
+
+import { MessageCounterComponent } from './message-counter.component';
+
+import { ChatRoutingModule } from './chat-routing.module';
 
 import { MomentModule } from 'angular2-moment';
 
-//ChatRoutingModule,
 @NgModule({
     imports: [
         CommonModule,
         SharedModule,
         MomentModule,
+        ChatRoutingModule,
     ],
     declarations: [
         ChatWindowComponent,
         ChatMessageComponent,
+        MessageCounterComponent,
     ],
     exports: [
         ChatWindowComponent,
         ChatMessageComponent,
+        MessageCounterComponent,
     ]
 })
 export class ChatModule {
---
+++ frontend/src/app/chat/message-counter.component.ts
@@ -0,0 +1,57 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+
+import { Thread, Message } from "../model/chat";
+import { ThreadService } from '../model/thread.service';
+import { MessageService } from '../model/message.service';
+
+import * as _ from 'lodash';
+
+@Component({
+  selector: 'message-count',
+  templateUrl: './message-counter.component.html',
+  styleUrls: ['./message-counter.component.css']
+})
+
+export class MessageCounterComponent implements OnInit {
+  unreadMessagesCount: number;
+
+  constructor(
+    private _router: Router,
+    private _msgService: MessageService,
+    private _threadService: ThreadService
+  ) {}
+
+  ngOnInit() {
+    this._msgService.messages
+      .combineLatest(
+        this._threadService.currentThread,
+        (messages: Message[], currentThread: Thread) => [currentThread, messages] )
+
+      .subscribe(([currentThread, messages]: [Thread, Message[]]) => {
+        this.unreadMessagesCount =
+          _.reduce(
+            messages,
+            (sum: number, m: Message) => {
+              const messageIsInCurrentThread: boolean = m.thread &&
+                currentThread &&
+                (currentThread.id === m.thread.id);
+              // note: in a "real" app you should also exclude
+              // messages that were authored by the current user b/c they've
+              // already been "read"
+              if (m && !m.isRead && !messageIsInCurrentThread) {
+                sum = sum + 1;
+              }
+              return sum;
+            }, 0);
+      });
+  }
+
+  /*public viewCourse(courseId:string):void {
+    this._router.navigate(['/course', courseId]);
+  }
+
+  public viewSchool(schoolId:string):void {
+    this._router.navigate(['/school', schoolId]);
+  }*/
+}
